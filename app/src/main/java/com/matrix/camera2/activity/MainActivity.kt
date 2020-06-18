4848e88a6d7773ce9aa4f09b94ddb5b8633013fa
xiexude <txsfmg@163.com>
2020-06-18 17:13:59 +0800

    新增：权限判断

--- app/src/main/java/com/matrix/camera2/activity/MainActivity.kt
+++ app/src/main/java/com/matrix/camera2/activity/MainActivity.kt
@@ -1,18 +1,24 @@
 package com.matrix.camera2.activity
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Intent
-import android.view.View
+import android.net.Uri
+import android.util.Log
+import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 import com.matrix.camera2.R
 import com.matrix.camera2.decoration.LinearDecoration
+import com.matrix.camera2.utils.ToastUtil
+import com.tbruyelle.rxpermissions2.RxPermissions
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
     private val strList = listOf("相机", "图库", "β射线", "等效原理", "自旋1/2")
-    private lateinit var adapter : BaseQuickAdapter<String, BaseViewHolder>
+    private lateinit var adapter: BaseQuickAdapter<String, BaseViewHolder>
 
     override fun getLayoutId(): Int {
         return R.layout.activity_main
@@ -37,10 +43,10 @@ class MainActivity : BaseActivity() {
         }
         adapter.onItemClickListener =
             BaseQuickAdapter.OnItemClickListener { _, _, position ->
-                when(position){
-                    0 ->{
-                        val intent = Intent(this@MainActivity, CameraActivity::class.java)
-                        startActivity(intent)
+                when (position) {
+                    0 -> {
+                        requestCameraStorage()
+                        ActivityCompat.shouldShowRequestPermissionRationale(this,"")
                     }
                 }
             }
@@ -48,7 +54,7 @@ class MainActivity : BaseActivity() {
     }
 
     private fun initRecycler() {
-        recycler_main.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+        recycler_main.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
         val decoration = LinearDecoration.Builder().apply {
             left = 80
@@ -56,5 +62,36 @@ class MainActivity : BaseActivity() {
             top = 70
         }.build()
         recycler_main.addItemDecoration(decoration)
+    }
+
+    // 申请相机、存储权限
+    @SuppressLint("CheckResult")
+    private fun requestCameraStorage() {
+        val rxPermissions = RxPermissions(this)
+        rxPermissions.setLogging(true)
+        rxPermissions.requestEach(Manifest.permission.CAMERA)
+            .subscribe { permission ->
+                if (permission.name == (Manifest.permission.CAMERA)) {
+                    if (permission.granted) {
+                        val intent = Intent(this@MainActivity, CameraActivity::class.java)
+                        startActivity(intent)
+                    } else if (permission.shouldShowRequestPermissionRationale) {
+                        ToastUtil.showToast(this@MainActivity, "我再出弹出的选择框")
+                        Log.d("xxd", "拒绝没禁止弹框")
+                    } else {
+                        ToastUtil.showToast(this@MainActivity, "我应该去setting")
+                        Log.d("xxd", "禁止弹框")
+                        val intent = Intent()
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        intent.action = "android.settings.APPLICATION_DETAILS_SETTINGS"
+                        intent.data = Uri.fromParts(
+                            "package",
+                            this.packageName,
+                            null
+                        )
+                        this.startActivity(intent)
+                    }
+                }
+            }
     }
 }
