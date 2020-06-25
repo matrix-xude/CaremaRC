45e33a84fe77114d138f87459a4279e2394ef449
xxd <txgsqy@163.com>
2020-06-25 16:35:26 +0800

    view animation

--- app/src/main/java/com/matrix/camera2/activity/AnimationActivity.kt
+++ app/src/main/java/com/matrix/camera2/activity/AnimationActivity.kt
@@ -1,10 +1,10 @@
 package com.matrix.camera2.activity
 
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
+import android.graphics.BitmapFactory
+import android.graphics.drawable.Animatable
+import android.graphics.drawable.AnimationDrawable
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
-import android.view.animation.TranslateAnimation
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import com.chad.library.adapter.base.BaseQuickAdapter
@@ -22,7 +22,8 @@ import kotlinx.android.synthetic.main.item_animation.view.*
  */
 class AnimationActivity : BaseActivity() {
 
-    val strList = listOf("translate", "rotate", "scale", "gradual", "mixture", "stop")
+    val strList =
+        listOf("translate", "scale", "alpha", "rotate", "mixture", "stop", "animation-list")
     lateinit var baseQuickAdapter: BaseQuickAdapter<String, BaseViewHolder>
 
     override fun getLayoutId(): Int {
@@ -32,8 +33,8 @@ class AnimationActivity : BaseActivity() {
     override fun initView() {
         super.initView()
         initRecycler()
-        iv_vector.setOnClickListener{
-            ToastUtil.showToast(this,"我被点击了")
+        iv_vector.setOnClickListener {
+            ToastUtil.showToast(this, "我被点击了")
         }
     }
 
@@ -59,6 +60,13 @@ class AnimationActivity : BaseActivity() {
         baseQuickAdapter.bindToRecyclerView(recycler_animation)
         baseQuickAdapter.setNewData(strList)
 
+        initClick()
+    }
+
+    /**
+     * 点击事件的初始化
+     */
+    private fun initClick() {
         baseQuickAdapter.setOnItemClickListener { _, _, position ->
             when (position) {
                 0 -> {
@@ -66,8 +74,41 @@ class AnimationActivity : BaseActivity() {
                         iv_vector.startAnimation(it)
                     }
                 }
-                5 ->{
+                1 -> {
+                    AnimationUtils.loadAnimation(this, R.anim.scale_1).also {
+                        iv_vector.startAnimation(it)
+                    }
+                }
+                2 -> {
+                    AnimationUtils.loadAnimation(this, R.anim.alpha_1).also {
+                        iv_vector.startAnimation(it)
+                    }
+                }
+                3 -> {
+                    AnimationUtils.loadAnimation(this, R.anim.rotate_1).also {
+                        iv_vector.startAnimation(it)
+                    }
+                }
+                4 -> {
+                    AnimationUtils.loadAnimation(this, R.anim.set_1).also {
+                        iv_vector.startAnimation(it)
+                    }
+                }
+                5 -> {
                     iv_vector.clearAnimation()
+                }
+                6 -> {
+//                    iv_vector.setBackgroundResource(R.drawable.animation_list)
+//                    val animation = iv_vector.background
+//                    if (animation is Animatable)
+//                        animation.start()
+                    iv_vector.setImageBitmap(null)
+
+                    iv_vector.setBackgroundResource(R.drawable.animation_list)
+                    val animation = iv_vector.background
+                    if (animation is AnimationDrawable)
+                        animation.start()
+
                 }
             }
         }
